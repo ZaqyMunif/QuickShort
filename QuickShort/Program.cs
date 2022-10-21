9cb912094d734c60848859e7881bba252ccd0ca1
ZaqyMunif <zaqymunif17@gmail.com>
2022-10-21 16:54:10 +0700

    Add project files.

---
+++ QuickShort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
