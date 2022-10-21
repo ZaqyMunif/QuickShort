970a47ae529113781cff7171ea17ab23fa43f562
ZaqyMunif <zaqymunif17@gmail.com>
2022-10-21 17:11:10 +0700

    add method input

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -11,8 +11,26 @@ namespace QuickShort
         //array of integers to hold values
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparasion
-        private int mov_count = 0;
+        private int mov_count = 0; // number of data movements
 
+        // Number of elements in array 
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n========================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("\n========================");
+        }
     }
 }
